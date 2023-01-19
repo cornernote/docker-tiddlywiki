e32a870de1d89cb5c5d0c82980574ef0c1459b61
Brett O'Donnell <cornernote@gmail.com>
2023-01-19 21:42:31 +1030

    load docker-settings when setting up

--- build/run-tiddlywiki.sh
+++ build/run-tiddlywiki.sh
@@ -4,6 +4,8 @@ set -e
 if [ ! -d /app/wikis ]; then
   mkdir /app/wikis
   cp -r /usr/local/tiddlywiki/plugins/OokTech/Bob/MultiUserWiki /app/wikis/BobWiki
+  rm /app/wikis/BobWiki/settings/settings.json
+  mv /app/wikis/BobWiki/settings/docker-settings.json /app/wikis/BobWiki/settings/settings.json
 fi
 
 node /usr/local/tiddlywiki/tiddlywiki wikis/BobWiki --wsserver
